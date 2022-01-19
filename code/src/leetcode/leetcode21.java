f9188635762b267fd9b3a9c7bb27fe5ba705d995
oshacker <xjtu_yuanwei@163.com>
2022-01-19 10:45:27 +0800

    leetcode21 合并两个有序链表 增加不使用虚拟头节点和递归写法

--- code/src/leetcode/leetcode21.java
+++ code/src/leetcode/leetcode21.java
@@ -10,7 +10,72 @@ public class leetcode21 {
         }
     }
 
+    // 总结：
+    // 时间复杂度：O(m+n)
+    // 空间复杂度：mergeTwoLists1、mergeTwoLists2、mergeTwoLists3为O(1)，mergeTwoLists为O(m+n)
+
+    // 推荐写法：
+    // 如果空间复杂度可以为O(m+n)，推荐mergeTwoLists递归写法
+    // 如果空间复杂度为O(1)，推荐mergeTwoLists1使用虚拟头节点写法
+
+    // 递归
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    // 不使用虚拟头节点
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode cur = null,head = null;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                if (cur == null) {
+                    head = l1;
+                    cur = l1;
+                } else {
+                    cur.next = l1;
+                    cur = cur.next;
+                }
+                l1 = l1.next;
+            } else {
+                if (cur == null) {
+                    head = l2;
+                    cur = l2;
+                } else {
+                    cur.next = l2;
+                    cur = cur.next;
+                }
+                l2 = l2.next;
+            }
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return head;
+    }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
         while (l1 != null || l2 != null) {
